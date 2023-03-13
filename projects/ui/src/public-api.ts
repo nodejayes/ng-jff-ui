55c001a7699ee830331013f1cee83ff3288aa33d
= <=>
2023-03-13 14:23:11 +0100

    init

---
+++ projects/ui/src/public-api.ts
@@ -0,0 +1,8 @@
+/*
+ * Public API Surface of ui
+ */
+
+export * from './lib/app-layout/app.layout.component';
+export * from './lib/card/card.component';
+export * from './lib/icon/menu.icon.component';
+export * from './lib/icon/close.icon.component';
