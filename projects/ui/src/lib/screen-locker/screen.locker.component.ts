ea8e3fd8d01a1407e32e34682bc8c9d98e88e9b0
Markus <markusmorgenstern87@outlook.de>
2023-03-31 12:28:01 +0200

    animate screen-locker

--- projects/ui/src/lib/screen-locker/screen.locker.component.ts
+++ projects/ui/src/lib/screen-locker/screen.locker.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { NgIf } from '@angular/common';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'jff-screen-locker',
@@ -7,6 +8,36 @@ import { NgIf } from '@angular/common';
   styleUrls: ['screen.locker.component.css'],
   standalone: true,
   imports: [NgIf],
+  animations: [
+    trigger('screenLocker', [
+      transition(':enter', [
+        style({
+          transform: 'translateY(0)',
+          opacity: 0,
+        }),
+        animate(
+          '250ms',
+          style({
+            transform: 'translateY(0)',
+            opacity: 1,
+          })
+        ),
+      ]),
+      transition(':leave', [
+        style({
+          transform: 'translateY(0)',
+          opacity: 1,
+        }),
+        animate(
+          '400ms',
+          style({
+            transform: 'translateY(-100%)',
+            opacity: 0,
+          })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class ScreenLockerComponent {
   @Input()
